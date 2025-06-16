3c8ea84b1ec6d7447315da8f42a05a3001f22596
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-06-16 18:54:29 -0300

    Finalizando o trabalho do professor Hélio, Sistema Ordem de Serviço

--- ModelagemDeBancoDeDadosI/modelos/atividade/createTablesOS.sql
+++ ModelagemDeBancoDeDadosI/modelos/atividade/createTablesOS.sql
@@ -1,8 +1,8 @@
 select version();
 
-create database ordemservico;
+create database ordem_de_servico;
 
-use ordemservico;
+use ordem_de_servico;
 
 show tables;
 
@@ -13,44 +13,9 @@ create table cliente(
     cli_email varchar(255) not null
 );
 
-create table cli_endereco(
-	cod_endereco int,
-	logradouro varchar(255) not null,
-    cidade varchar(255) not null,
-    estado varchar(2) not null,
-    cep varchar(8) not null,
-    bairro varchar(255) not null,
-    numero int not null,
-    FOREIGN KEY (cod_endereco) REFERENCES cliente(cli_codigo)
-);
-
-
-create table equipamento(
-	equi_codigo varchar(8) primary key,
-	equi_descricao text,
-    equi_marca varchar(50) not null,
-    equi_ano_fabri date not null,
-    equi_detal_config text,
-    equi_obs text,
-	fk_cli_codigo INT,
-    FOREIGN KEY (fk_cli_codigo) REFERENCES cliente(cli_codigo)
-);
-
-create table ordemServico(
-	os_codigo int auto_increment primary key,
-	os_data date not null,
-    os_data_fechamento date not null,
-    fk_cli_codigo int not null,
-    fk_equi_codigo int not null,
-    fk_tec_codigo int not null,
-    FOREIGN KEY (fk_cli_codigo) REFERENCES cliente(cli_codigo),
-    FOREIGN KEY (fk_equi_codigo) REFERENCES equipamento(equi_codigo),
-	FOREIGN KEY (fk_tec_codigo) REFERENCES tecnico(tec_codigo)
-);
-
 create table servico(
 	ser_codigo int auto_increment primary key, 
-    ser_descricao text,
+    ser_descricao varchar(255),
     ser_horas_de_aplicacao time,
     ser_valor decimaL(5,2) not null
 );
@@ -58,11 +23,60 @@ create table servico(
 create table item(
 	item_codigo int primary key auto_increment,
     item_valor decimal(5,2) not null,
-    item_desc text
+    item_desc varchar(255)
 );
 
 create table tecnico(
 	tec_codigo int primary key auto_increment,
     tec_nome varchar(50) not null,
     tec_tell varchar(11) not null unique
+);
+
+create table cli_endereco(
+	logradouro varchar(255) not null,
+    cidade varchar(255) not null,
+    estado varchar(2) not null,
+    cep varchar(8) not null,
+    bairro varchar(255) not null,
+    numero int not null,
+    fk_cod_endereco int,
+    FOREIGN KEY (fk_cod_endereco) REFERENCES cliente(cli_codigo)
+);
+
+
+create table equipamento(
+	equi_codigo varchar(8) primary key,
+	equi_descricao varchar(255),
+    equi_marca varchar(50) not null,
+    equi_ano_fabri date not null,
+    equi_detal_config varchar(255),
+    equi_obs varchar(255),
+	fk_cli_codigo INT,
+    FOREIGN KEY (fk_cli_codigo) REFERENCES cliente(cli_codigo)
+);
+
+create table ordem_servico(
+	os_codigo int auto_increment primary key,
+	os_data date not null,
+    os_data_fechamento date not null,
+    fk_cli_codigo int,
+    fk_equi_codigo varchar(8),
+    fk_tec_codigo int,
+    FOREIGN KEY (fk_cli_codigo) REFERENCES cliente(cli_codigo),
+    FOREIGN KEY (fk_equi_codigo) REFERENCES equipamento(equi_codigo),
+	FOREIGN KEY (fk_tec_codigo) REFERENCES tecnico(tec_codigo)
+);
+
+create table os_servico(
+	fk_os_codigo int,
+	fk_ser_codigo int,
+	FOREIGN KEY (fk_os_codigo) REFERENCES ordem_servico(os_codigo),
+    FOREIGN KEY (fk_ser_codigo) REFERENCES servico(ser_codigo)
+);
+
+create table os_item(
+	fk_os_codigo int,
+    fk_item_codigo int,
+    FOREIGN KEY (fk_os_codigo) REFERENCES ordem_servico(os_codigo),
+    FOREIGN KEY (fk_os_codigo) REFERENCES item(item_codigo)
 );
